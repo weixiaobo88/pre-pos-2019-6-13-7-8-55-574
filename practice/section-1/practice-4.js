3a61cd4f4c16a911560ce3e61d804bd9efffa241
Zhaoyu <weixiaobo88@gmail.com>
2019-07-05 14:33:25 +0800

    finish section-1practice-4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,18 @@
-'use strict';
+"use strict";
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let result = [];
+
+  for (let index = 0; index < collectionA.length; index++) {
+    const element = collectionA[index].key;
+    const value = objectB.value;
+    if (isElementInCollection(value, element)) {
+      result.push(element);
+    }
+  }
+  return result;
+}
+
+function isElementInCollection(collection, element) {
+  return collection.indexOf(element) > -1;
 }
