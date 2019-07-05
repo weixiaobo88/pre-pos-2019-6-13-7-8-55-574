2fc0d81ecec347257f6df72d829bc7b003762cc1
Zhaoyu <weixiaobo88@gmail.com>
2019-07-05 14:34:19 +0800

    finish section-2practice-1

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,34 @@
-'use strict';
+"use strict";
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let result = {};
+  for (let index = 0; index < collection.length; index++) {
+    const element = collection[index];
+
+    if (findElementInObejct(result, element)) {
+      result = { ...result, [element]: result[element] + 1 };
+    } else {
+      result[element] = 1;
+    }
+  }
+  return mapObjectToCollection(removeSingleElement(result));
+}
+
+function findElementInObejct(object, element) {
+  return object[element];
+}
+
+function mapObjectToCollection(object) {
+  return Object.keys(object).map(item => ({ key: item, count: object[item] }));
+}
+
+function removeSingleElement(object) {
+  const keys = Object.keys(object);
+  for (let index = 0; index < keys.length; index++) {
+    const element = keys[index];
+    if (object[element].count === 1) {
+      delete object[element];
+    }
+  }
+  return object;
 }
